7012e15315c31002b244b0009b0e0a388a9d4ed5
anph65 <131370020+anph65@users.noreply.github.com>
2023-04-21 01:40:54 +0900

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import math
+
+# Define the style for the circular buttons
+button_style = "border-radius: 50%; height: 50px; width: 50px; font-size: 20px;"
 
 # Title of the app
 st.title("Simple Calculator by Huu An")
@@ -6,17 +10,26 @@ st.title("Simple Calculator by Huu An")
 # User input for two numbers and operation
 num1 = st.number_input("Enter the first number", value=0)
 num2 = st.number_input("Enter the second number", value=0)
-operation = st.selectbox("Select an operation", ["Addition", "Subtraction", "Multiplication", "Division"])
+
+# Define the buttons
+add_button = st.button(label="+", key="add", style=button_style)
+subtract_button = st.button(label="-", key="subtract", style=button_style)
+multiply_button = st.button(label="x", key="multiply", style=button_style)
+divide_button = st.button(label="÷", key="divide", style=button_style)
 
 # Perform the selected operation
-if operation == "Addition":
+if add_button:
     result = num1 + num2
-elif operation == "Subtraction":
+elif subtract_button:
     result = num1 - num2
-elif operation == "Multiplication":
+elif multiply_button:
     result = num1 * num2
-elif operation == "Division":
-    result = num1 / num2
+elif divide_button:
+    if num2 == 0:
+        st.write("Error: Division by zero!")
+    else:
+        result = num1 / num2
 
 # Display the result
-st.write("The answer is:", result)
+if "result" in locals():
+    st.write("The answer is:", result)
